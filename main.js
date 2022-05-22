a5a86f43411f28f52a7bd1efb7cdc3c6d6558a47
Kashif-Gulzar <106018529+Kashif-Gulzar@users.noreply.github.com>
2022-05-22 17:41:03 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const Courses=[{
+    Id:"1",
+    title:"Web Development",
+    description:"Principal areas of study within Computer Science include artificial intelligence, computer systems and networks, security, database systems, human computer interaction, vision and graphics, numerical analysis, programming languages, software engineering, bioinformatics and theory of computing.",
+    course1:"HTML",
+    course2:"CSS",
+    course3:"Javascript",
+    image:require("../.././asset/web.jpg"),
+
+    price:5000
+    
+},
+{
+    Id:"2",
+    title:"Web Graphic Designing",
+    description:"Principal areas of study within Computer Science include artificial intelligence, computer systems and networks, security, database systems, human computer interaction, vision and graphics, numerical analysis, programming languages, software engineering, bioinformatics and theory of computing.",
+    course1:"HTML",
+    course2:"CSS",
+    course3:"Javascript",
+    image:require("../.././asset/realgraphic.jpg"),
+    price:5000
+},
+{
+    Id:"3",
+    title:"Android",
+    description:"Principal areas of study within Computer Science include artificial intelligence, computer systems and networks, security, database systems, human computer interaction, vision and graphics, numerical analysis, programming languages, software engineering, bioinformatics and theory of computing.",
+    course1:"HTML",
+    course2:"CSS",
+    course3:"Javascript",
+    image:require("../.././asset/android.jpg"),
+
+    price:5000
+},
+];
+
+export default Courses
